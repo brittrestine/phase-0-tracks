71d3b728696c9e86d9ed31061973e5a8191953f4
Brittany Restine <brittrestine@gmail.com>
2017-05-15 21:35:19 -0500

    correct methods

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,36 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+p "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+p "zom".insert(1, 'o')
 # => “zoom”
 
-# "enhance".<???>
+p "enhance". center(15)
 # => "    enhance    "
 
 # "Stop! You’re under arrest!".<???>
 # => "STOP! YOU’RE UNDER ARREST!"
-
-# "the usual".<???>
+a = "suspects"
+p a.prepend("the usual ")
 #=> "the usual suspects"
 
-# " suspects".<???>
+a = "the usual"
+p " suspects".prepend(a)
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+p "The case of the disappearing last letter".slice!(0..38)
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
-# => "he mystery of the missing first letter"
-
-# "Elementary,    my   dear        Watson!".<???>
+p "The mystery of the missing first letter".slice!(1..39)
+# => "he mystery of the missing first letter
+p "Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
-# => 122 
+p "z".ord
+# => 122
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+p "How many times does the letter 'a' appear in this string?".count "a"
 # => 4
