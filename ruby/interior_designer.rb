c7c4cae25fbfe5d675602d6b2e290601e078a702
Brittany Restine <brittrestine@gmail.com>
2017-05-16 10:34:20 -0500

    hash for interior design app

---
+++ ruby/interior_designer.rb
@@ -0,0 +1,21 @@
+job_application = {
+  name: "britt",
+  address: "austin, tx",
+  email: "britt@gmail.com",
+  phone: "7735121929",
+  blue_shade: "light blue",
+  wallpaper: "paisley",
+  ombre: "so last season"
+}
+
+=begin
+job_application[:last_name] = "restine"
+my_name = :name
+job_application[:name]
+job_application[:name] + job_application[:address]
+job_application.clear
+job_application.length
+job_application.delete(:name)
+job_application.has_key?(:name)
+job_application
+=end
