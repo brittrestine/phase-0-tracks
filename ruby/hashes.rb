a6fe44f9153095e7061a7dfaa8158b3d822baa72
Brittany Restine <brittrestine@gmail.com>
2017-05-16 11:42:30 -0500

    designer information

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -6,6 +6,7 @@ pseudocode
   -favorit color
   -likes plants
   -update
+-update data if needed or user can say none to exit
 -convert designers input to data type
 -print out hash
 =end
@@ -30,8 +31,27 @@ puts "would you like plants?(yes or no)"
 plants = gets.chomp
 designer_info[:plants] = plants
 
-puts "would you like to update anything?"
+puts "Anything you would to update?(none to exit)"
 update = gets.chomp
-designer_info[:update] = update
+if update == "name"
+ puts "update to"
+ name_update = gets.chomp
+ designer_info[:name] = name_update
+elsif update == "decor theme"
+  puts "update to"
+  decor_theme_update = gets.chomp
+  designer_info[:decor_theme] = decor_theme_update
+elsif update == "favorite color"
+  puts "update to"
+  fav_color_update = gets.chomp
+  designer_info[:fav_color] = fav_color_update
+elsif update == "plants"
+  puts "update to"
+  plants_update = gets.chomp
+  designer_info[:plants] = plants_update
+else update == "none"
+  puts "Thank you for you information"
+end
+
 
 p designer_info
