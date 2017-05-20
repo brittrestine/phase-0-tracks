44a31031b8a593b87d320add0ae7c4d4142b5741
Brittany Restine <brittrestine@gmail.com>
2017-05-20 18:40:40 -0500

    alias manager complete

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -11,51 +11,50 @@
 
 =end
 
-def alias_manager(first_name, last_name)
-  name = (last_name + first_name)
+def alias_manager(name)
   name = name.downcase
+  name = name.split(" ").reverse.join(" ")
+  name = name.split("")
   index = 0
-  alphabet = "bcdfghjklmnpqrstvwqyz"
+  consonant = "bcdfghjklmnpqrstvwqyz"
   vowel = "aeiou"
-  while index < name.length
-    if index_of_letter =  vowel.index(name[index])
-     new_letter = vowel[index_of_letter + 1]
-     name[index] = new_letter
-   else index_of_letter = alphabet.index(name[index])
-      new_letter = alphabet[index_of_letter + 1]
+   while index < name.length
+    if
+      !consonant.index(name[index]) && !vowel.index(name[index])
+
+    elsif
+      index_of_letter =  vowel.index(name[index])
+      new_letter = vowel[index_of_letter + 1]
+      name[index] = new_letter
+    else
+      index_of_letter = consonant.index(name[index])
+      new_letter = consonant[index_of_letter + 1]
       name[index]= new_letter
     end
     index += 1
   end
-  name
-  yield(name)# Problem! I want to pass the result of this method to my hash for the value
+  name.join("")
 end
 
 #user interface
 
-#aliass = {}#Problem! I really want my data structure to be a hash so it can hold the original 'name' as the key and the 'name_1' as the value. I dont know how to add the value from a method
+name_catcher = {}
 
 loop_counter = 0
 loop do
-  puts "To get an alias enter a first name?('quit' when finished)"
+
+puts "To get an alias enter with no spaces a name?(write'quit' when finished)"
   name_1 = gets.chomp
-  #aliass << name_1#Problem I want to pass this to my hash as the key
-
-
-  if name_1 == "quit"
-    puts "Thanks for using alias manager!"
-    break
-  end
-
-  puts "Enter a last name"
-  name_2 = gets.chomp
-  #aliass << name_2#Problem I want to pass this to my hash as the key
+  break if name_1 == "quit"
+  name_catcher[name_1]= alias_manager(name_1)
 
   loop_counter =+ 1
 
- #Problem! I want this to print outside my loop so it doesn't print after you enter a name, but if I copy and paste this outside the loop it cant find the 'name' variale. The block wont be necessary if I could pass the value to the hash
-puts alias_manager(name_1, name_2) { |name| puts "#{name.capitalize} is actually #{name_1.capitalize} #{name_2.capitalize}"}
+  alias_manager(name_1)
+
 
 end
 
-
+name_catcher.each do |original_name, new_name|
+  puts "#{new_name.split.map{|i| i.capitalize}.join(' ')} is acctually #{original_name}"
+end
