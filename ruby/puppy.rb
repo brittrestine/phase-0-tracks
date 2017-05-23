9cf0b3e1441c6818e81f0b5f17f79f6e49adfba9
Brittany Restine <brittrestine@gmail.com>
2017-05-23 11:43:34 -0500

    puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+SPECIES ------------------------
+Puppies
+
+CHARACTERISTICS ----------------
+paws: 4
+tale: varies in size
+snout: varies in size
+Friendly: yes
+Name: varies
+Color: varies
+
+BEHAVIOR -----------------------
+Jump
+Playful
+Rambunctious
+Curious
