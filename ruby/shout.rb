8c1ac8aa7a03cc356a63359af2cc7e7c7f3c2011
Brittany Restine <brittrestine@gmail.com>
2017-05-24 11:14:34 -0500

    shout module complete

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,14 @@
 module Shout
-   # we'll put some methods here soon, but this code is fine for now!
+
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yell_happily(words)
+    words + " " + "yay!!!" + "<3 =-)"
+  end
+
 end
+
+puts Shout.yell_angrily("This sucks")
+puts Shout.yell_happily("I'm so happy")
