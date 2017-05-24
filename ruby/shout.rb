6a02f8a343f432725d0018f73747dfa17345c12c
Brittany Restine <brittrestine@gmail.com>
2017-05-24 11:37:39 -0500

    Shouts module mixin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,4 @@
+=begin
 module Shout
 
   def self.yell_angrily(words)
@@ -12,3 +13,23 @@ end
 
 puts Shout.yell_angrily("This sucks")
 puts Shout.yell_happily("I'm so happy")
+=end
+
+module Shouts
+  def yelling(words)
+    "OMG!!!" + words + "!!!!!"
+  end
+end
+
+class Programmers
+  include Shouts
+end
+
+class Sports_Fans
+  include Shouts
+end
+
+new_programmer = Programmers.new
+puts new_programmer.yelling("look what I did")
+new_sports_fan = Sports_Fans.new
+puts new_sports_fan.yelling("Yes, we are winning")
