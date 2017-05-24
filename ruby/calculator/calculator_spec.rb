a7ff064f114f540620d4169abce9b8d43af645ee
Brittany Restine <brittrestine@gmail.com>
2017-05-24 16:50:25 -0500

    syntax fix

--- ruby/calculator/calculator_spec.rb
+++ ruby/calculator/calculator_spec.rb
@@ -7,7 +7,6 @@
 # 'describe' block for each group of tests
   # 'it' block for each individual test
     # expect(<YOUR CODE>).to eq <RESULT>
-
 require_relative 'calculator'
 
 describe Calculator do
