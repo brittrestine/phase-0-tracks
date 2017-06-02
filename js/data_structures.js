503e1461222f4a955fb49c723273bab22a007408
Brittany Restine <brittrestine@gmail.com>
2017-06-02 14:48:41 -0500

    arrays to object

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,3 +1,5 @@
+
+
 var horse = ["candy", "noname", "champ", "snowball"];
 
 var color = ["red", "blue", "green", "yellow"];
@@ -6,6 +8,15 @@ horse.push("flapjack");
 
 color.push("purple");
 
-console.log(horse);
+//console.log(horse);
 
-console.log(color);
+//console.log(color);
+
+var myobj = {};
+
+for (var i = 0; horse.length > i; i++){
+    myobj[horse[i]] = color[i];
+  }
+
+
+console.log(myobj);
