3ce2936f3f80c4805c4336d77b8d19cd45c57579
Brittany Restine <brittrestine@gmail.com>
2017-06-02 13:54:29 -0500

    horse and color array

---
+++ js/data_structures.js
@@ -0,0 +1,11 @@
+var horse = ["candy", "noname", "champ", "snowball"];
+
+var color = ["red", "blue", "green", "yellow"];
+
+horse.push("flapjack");
+
+color.push("purple");
+
+console.log(horse);
+
+console.log(color);
