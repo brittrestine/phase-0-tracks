df5f1fbf387e90f294b5c2daa4bf8b186379a0e6
Brittany Restine <brittrestine@gmail.com>
2017-06-02 10:45:58 -0500

    compare to objects

--- js/algo.js
+++ js/algo.js
@@ -1,10 +1,12 @@
+//Pseudocode for printlongest function
 // Create a longest word in array method
   // This method will take in a array of strings or phrases as an argument
   // look through that array at every word or pharse
   // compare the word lengths of each one
   // the largest word or phrase should be returned
 
-function long(arr){
+/*
+function printlongest(arr){
 
 var lgth = 0;
 var longest;
@@ -18,6 +20,46 @@ for(var i = 0; i < arr.length; i++){
 }
 console.log(longest);
 }
+*/
+
+// Pseudocode for compare
+//Create a function that takes in two key-value pairs
+  //first compare the keys. Call both of the keys.
+    //if they are equal to one another then the return should be true.
+    //if they are not equal the return should be false.
+  // Second campare the values.
+    //Call both of the values. I
+    //If they are equal to one another then return should be true.
+    //If they are not equal to one another then the return should be false
+/*
+    function compare(kv1, kv2) {
+      if (kv1.name == kv2.name) {
+        console.log(true);
+      } else if (kv1.age == kv2.age) {
+        console.log(true);
+      }  else if (kv1.name != kv2.name) {
+        console.log(false);
+      } else if (kv1.age != kv2.age) {
+        console.log(false);
+      }
+    }
+*/
+
+//***************driver code**********************
+
+/* printlongest function driver code,
+
 var arr = ["I like ice cream", "Yes I do", "yepper"];
 
-long(arr);
+printlongest(arr);
+*/
+
+/* compare driver code,
+
+var ryan = {name: "ryan", age: 29};
+
+var britt = {name: "britt", age: 29};
+
+compare(ryan, britt);
+*/
+
