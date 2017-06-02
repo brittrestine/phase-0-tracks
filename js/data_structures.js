1b63152fab7817006c97a28ea45878925abb6e4b
Brittany Restine <brittrestine@gmail.com>
2017-06-02 16:31:38 -0500

    new car constructor

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,5 +1,4 @@
-
-
+/*
 var horse = ["candy", "noname", "champ", "snowball"];
 
 var color = ["red", "blue", "green", "yellow"];
@@ -8,9 +7,9 @@ horse.push("flapjack");
 
 color.push("purple");
 
-//console.log(horse);
+console.log(horse);
 
-//console.log(color);
+console.log(color);
 
 var myobj = {};
 
@@ -22,5 +21,42 @@ for (var i = 0; color.length > i; i++){
   }
 }
 
-
 console.log(myobj);
+*/
+
+//var car = {model: valiant, year: 1968, color: tan};
+
+function Car(make, year, color) {
+
+  console.log("Our new car:", this);
+
+  this.make = make;
+  this.year = year;
+  this.color = color;
+
+  this.running = function() {console.log("vroom");};
+
+  console.log("CAR INITIALIZATION COMPLETE");
+}
+
+console.log("Lets build a car...");
+var anotherCar = new Car("Valiant", 1968, "tan");
+console.log(anotherCar);
+console.log("This car is running:");
+anotherCar.running();
+console.log("-------------")
+
+console.log("lets build another car...");
+var yetanotherCar = new Car("Dodge", 2008, "red");
+console.log(yetanotherCar);
+console.log("This car is running:");
+yetanotherCar.running();
+console.log("-------------")
+
+console.log("lets build another car...");
+var yetyetanotherCar = new Car("Volvo", 2001, "gren");
+console.log(yetyetanotherCar);
+console.log("This car is running:");
+yetyetanotherCar.running();
+console.log("-------------")
+
