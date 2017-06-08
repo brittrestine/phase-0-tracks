79fdd16f87c259a45102ced2cbf94d94d077bb54
Brittany Restine <brittrestine@gmail.com>
2017-06-08 16:08:01 -0500

    update for house hold name and scientific name for user interface

--- databases/plant_tracker/plant_tracker.rb
+++ databases/plant_tracker/plant_tracker.rb
@@ -24,7 +24,7 @@ end
 loop_counter = 0
 
 loop do
-  puts "\nWould you like to look at all data, update, delete, create, or look at last entry.(enter: all, update, delete, create or last)(q to quit)"
+  puts "\nWould you like to look at all of the data, update data, delete data, create data, or look at last data entry.\n(enter: all, update, delete, create or last)(q to quit)"
     user_wants = gets.chomp
 
       if user_wants == "all"
@@ -33,23 +33,23 @@ loop do
         puts "\n#{plant['householdname']} is #{plant['scientificname']}"
         end
       end
-=begin
+
       if user_wants == "update"
         puts  "\nWhat is the house hold name of the plant you would like to update?"
           old_house_hold_name = gets.chomp
-        puts "\nDo you want to update the house hold name or the scientific name of the plant?(house hold name/ scientific name)"
+        puts "\nDo you want to update the house hold name or the scientific name of the plant?\n(house hold name / scientific name)"
           hhname_or_sname = gets.chomp
           if hhname_or_sname == "house hold name"
             puts "\nWhat would you like the new house hold name to be?"
             new_house_hold_name = gets.chomp
-            plantss = db.execute("UPDATE plants SET householdname='#{new_house_hold_name}' WHERE name='#{old_house_hold_name}'")
+            plants = db.execute("UPDATE plants SET householdname='#{new_house_hold_name}' WHERE householdname='#{old_house_hold_name}'")
           else hhname_or_sname == "scientific name"
-            puts "what do you want the scientific name to be updated to?"
+            puts "\nwhat do you want the scientific name to be updated to?"
             new_scientific_name = gets.chomp
-            plants = db.execute("UPDATE kittens SET scientificname='#{new_scientific_name}' WHERE name='#{old_house_hold_name}'")
+            plants = db.execute("UPDATE plants SET scientificname='#{new_scientific_name}' WHERE householdname='#{old_house_hold_name}'")
           end
         end
-
+=begin
         if user_wants == "delete"
           puts "\nWhat is the name of the plant you want to delete?"
           delete_name = gets.chomp
@@ -72,7 +72,7 @@ loop do
         end
       end
 =end
-    create_plant_data(db, h_h_n, s_n)
+   # create_plant_data(db, h_h_n, s_n)
 
       break if user_wants == "q"
 
