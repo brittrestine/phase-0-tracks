e6ebc1ffd53774124af8ae6627a7b90202ff045d
Brittany Restine <brittrestine@gmail.com>
2017-06-13 12:26:11 -0500

    fade in for photo, info appear, hover over list.

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -1,12 +1,23 @@
 $(document).ready(function() {
-  $("p").hide();
-  $("#bob-dylan").hide();
 
+  $("#bob-dylan").hide();
   $("#see-dylan").click(function() {
     $("#bob-dylan").fadeIn("slow");
   });
 
+  $("p").hide();
   $("#learn").click(function() {
     $("p").show();
   });
+
+  $( "li.fav" ).hover(function() {
+    $( this ).append( $( "<span> : MY FAVORITE ALBUM!!! <3 </span>" ) );
+  }, function() {
+    $( this ).find( "span:last" ).remove()});
+
+  $( "li.tied" ).hover(function() {
+    $( this ).append( $( "<span> : MY SECOND FAVORITE ALBUM..... </span>" ) );
+  }, function() {
+    $( this ).find( "span:last" ).remove()});
+
 });
